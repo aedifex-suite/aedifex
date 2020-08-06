66f7790ac5952b261f0650d51049e97b338757a0
kts of kettek (yan) <kettek1@kettek.net>
2020-08-06 07:08:14 -0700

    Expand schema

--- src/schemata/BestiaryEntrySchema.js
+++ src/schemata/BestiaryEntrySchema.js
@@ -11,12 +11,84 @@ const { NumberValuesSchema } = require('schemata/NumberValuesSchema')
   }
 })*/
 
+const BestiaryFeatSchema = schisma({
+  name: String,
+})
+
+const BestiaryLanguageSchema = schisma({
+  name: String,
+  distance: 0, // Only used if name == telepathy
+})
+
+const BestiarySkillSchema = schisma({
+  name: String,
+  value: Number,
+})
+
+const BestiaryDamageSchema = schisma({
+  dice: Number,
+  pips: Number,
+  bonus: Number,
+  types: [String],
+})
+
+const BestiaryMeleeSchema = schisma({
+  name: String,
+  attacks: Number,
+  tohit: Number,
+  damage: [BestiaryDamageSchema],
+})
+
+const BestiarySpellSchema = schisma({
+  name: String,
+  dc: {
+    $type: Number,
+    $required: false,
+  },
+  extra: String,
+  // TODO: summons
+})
+
 const BestiaryEntrySchema = schisma({
   type: 'bestiary',
-  core: {
-    hd: 1,
-    hitdie: 8,
+  description: '',
+  // Defense
+  "natural ac": Number,
+  hitdice: 1,
+  hitpips: 8,
+  fortitude: NumberValuesSchema,
+  reflex: NumberValuesSchema,
+  will: NumberValuesSchema,
+  dr: [{
+    $type: {
+      value: Number,
+      bypass: String,
+    },
+  }],
+  immune: [String],
+  resist: [{
+    $type: {
+      type: String,
+      value: Number,
+    }
+  }],
+  sr: NumberValuesSchema,
+  // Offense
+  speed: {
+    $type: Number,
+    $default: 30,
   },
+  melee: [BestiaryMeleeSchema],
+  "special attacks": [String],
+  "spell-like abilities": {
+    cl: Number,
+    concentration: Number,
+    constant: [BestiarySpellSchema],
+    "at will": [BestiarySpellSchema],
+    "3/day": [BestiarySpellSchema],
+    "1/day": [BestiarySpellSchema],
+  },
+  // Statistics
   "ability scores": {
     str: {
       $type: NumberValuesSchema,
@@ -42,7 +114,13 @@ const BestiaryEntrySchema = schisma({
       $type: NumberValuesSchema,
       $default: { value: 10 },
     },
-  }
+  },
+  bab: Number,
+  cmb: Number,
+  cmd: Number,
+  feats: [BestiaryFeatSchema],
+  skills: [BestiarySkillSchema],
+  languages: [BestiaryLanguageSchema],
 })
 
 module.exports = BestiaryEntrySchema
