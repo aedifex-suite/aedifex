06329c1a677ce08f15f990a0ae2c176b035f4a3f
kts of kettek (POWERQWACK) <kts@kettek.net>
2020-08-05 20:24:36 -0700

    Add utils alias

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,7 @@ module.exports = {
 			models: path.resolve(__dirname, 'src/models/'),
 			views: path.resolve(__dirname, 'src/views/'),
 			assets: path.resolve(__dirname, 'src/assets/'),
+			utils: path.resolve(__dirname, 'src/utils/'),
 		}
 	},
 	module: {
